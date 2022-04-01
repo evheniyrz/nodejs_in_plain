338a1f61d1f7fcef1b45d68c189adb90c7d289ab
evgenjnr <evgeniyr7@gmail.com>
2022-04-01 16:19:46 +0300

    git - lesson 20 - prepare deploy

--- server.js
+++ server.js
@@ -2,27 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const postRoutes = require('./routes/post-route');
 const contactsRoutes = require('./routes/contacts-routes');
 const postApiRoutes = require('./routes/api-post-routes');
 const createPath = require('./helpers/create-path');
 
 
-const dbUri = 'mongodb+srv://euhene:22ll56vv65tt@cluster0.vq1hm.mongodb.net/node-blog?retryWrites=true&w=majority';
-
 mongoose
-  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((resp) => console.log('===DB CONNECTION SUCCESSFUL==='))
   .catch((error) => console.log('DB CONNECTION ERROR', error));
 
 const app = express();
 
-const PORT = 3000;
-
 app.set('view engine', 'ejs');
 
-app.listen(PORT, (err) => {
-  err ? console.log('SERVER ERROR==>', err) : console.log(`Listening PORT==> ${PORT}`);
+app.listen(process.env.PORT, (err) => {
+  err ? console.log('SERVER ERROR==>', err) : console.log(`Listening PORT==> ${process.env.PORT}`);
 });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
